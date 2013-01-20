8a70d205635a09c84a02fce0061064f949225a95
Johnny Brown <jb@stafftacular.com>
2013-01-20 17:18:35 -0300

    remove unused dep

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.cgrand/parsley "0.9.1"]
                  [com.lithinos/amotoen "0.3.0-SNAPSHOT"]])
